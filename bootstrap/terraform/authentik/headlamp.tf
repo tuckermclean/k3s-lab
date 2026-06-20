5f9f0c5ace702152e91aab35e5bffdb3c357620c
Tucker McLean <me@tuckermclean.com>
2026-06-20 01:31:13 -0700

    feat(authentik): add proxy providers for juicefs.dcxxiv.com and s3.dcxxiv.com
    Register JuiceFS Dashboard and JuiceFS S3 Gateway as Authentik forward_single
    proxy apps so forwardAuth stops returning 404 for those hosts.

--- bootstrap/terraform/authentik/headlamp.tf
+++ bootstrap/terraform/authentik/headlamp.tf
@@ -37,12 +37,44 @@ resource "authentik_application" "longhorn" {
   meta_launch_url   = "https://longhorn.dcxxiv.com"
 }
 
+resource "authentik_provider_proxy" "juicefs_dashboard" {
+  name               = "JuiceFS Dashboard"
+  authorization_flow = data.authentik_flow.default_authorization.id
+  invalidation_flow  = data.authentik_flow.default_invalidation.id
+  mode               = "forward_single"
+  external_host      = "https://juicefs.dcxxiv.com"
+}
+
+resource "authentik_application" "juicefs_dashboard" {
+  name              = "JuiceFS Dashboard"
+  slug              = "juicefs-dashboard"
+  protocol_provider = authentik_provider_proxy.juicefs_dashboard.id
+  meta_launch_url   = "https://juicefs.dcxxiv.com"
+}
+
+resource "authentik_provider_proxy" "juicefs_s3" {
+  name               = "JuiceFS S3 Gateway"
+  authorization_flow = data.authentik_flow.default_authorization.id
+  invalidation_flow  = data.authentik_flow.default_invalidation.id
+  mode               = "forward_single"
+  external_host      = "https://s3.dcxxiv.com"
+}
+
+resource "authentik_application" "juicefs_s3" {
+  name              = "JuiceFS S3 Gateway"
+  slug              = "juicefs-s3"
+  protocol_provider = authentik_provider_proxy.juicefs_s3.id
+  meta_launch_url   = "https://s3.dcxxiv.com"
+}
+
 resource "authentik_outpost" "embedded" {
   name               = "authentik Embedded Outpost"
   type               = "proxy"
   protocol_providers = [
     authentik_provider_proxy.headlamp.id,
     authentik_provider_proxy.longhorn.id,
+    authentik_provider_proxy.juicefs_dashboard.id,
+    authentik_provider_proxy.juicefs_s3.id,
   ]
 
   lifecycle {
